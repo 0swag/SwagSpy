3bc6fdf8acdc4b9751b31505833ddcf79a5df70d
0swag <slav@insiberia.net>
2025-02-02 22:28:50 +0000

    Windows OS Version

--- client.c
+++ client.c
@@ -2,9 +2,36 @@
 #include <stdio.h>
 #include <strings.h>
 
-void driveEnum()
+char* sysInfo()
 {
-  char finalOutput[2048];
+  char cpuInfo[256];
+  
+  OSVERSIONINFO osvi;
+  ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+
+  char* finalOutput = (char*)malloc(2048);
+  char temp[2048];
+  char computerName[MAX_COMPUTERNAME_LENGTH + 1];
+  DWORD size = sizeof(computerName);
+  finalOutput[0] = '\0';
+
+  GetComputerName(computerName, &size);
+
+  snprintf(temp, sizeof(temp), "PC Name: %s\n", computerName);
+  strcat(finalOutput, temp);
+
+  osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+  GetVersionEx(&osvi);
+  snprintf(temp, sizeof(temp), "Windows %d.%d Build %d", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
+  strcat(finalOutput, temp);
+  
+  return finalOutput;
+}
+
+char* driveEnum()
+{
+  char* finalOutput = (char*)malloc(2048);
+  ZeroMemory(finalOutput, 2048);
   char temp[2048];
   char buf[MAX_PATH];
   int dType;
@@ -22,28 +49,28 @@ void driveEnum()
     switch(dType)
     {
       case DRIVE_UNKNOWN:
-        dTypeStr = "Unknown ❓";
+        dTypeStr = "Unknown";
         break;
       case DRIVE_NO_ROOT_DIR:
-        dTypeStr = "Invalid root directory ⚠️";
+        dTypeStr = "Invalid root directory";
         break;
       case DRIVE_REMOVABLE:
-        dTypeStr = "Removable drive 💾";
+        dTypeStr = "Removable drive";
         break;
       case DRIVE_FIXED:
-        dTypeStr = "Fixed drive (HDD/SSD, etc.) ";
+        dTypeStr = "Fixed drive (HDD/SSD, etc.)";
         break;
       case DRIVE_REMOTE:
-        dTypeStr = "Network drive 🌐";
+        dTypeStr = "Network drive";
         break;
       case DRIVE_CDROM:
-        dTypeStr = "CD-ROM 💿";
+        dTypeStr = "CD-ROM";
         break;
       case DRIVE_RAMDISK:
         dTypeStr = "RAM disk";
         break;
       default:
-        dTypeStr = "Unknown ❓";
+        dTypeStr = "Unknown";
         break;
     }
     snprintf(temp, sizeof(temp), "Drive: %s | Type: %s\n", drive, dTypeStr);
@@ -51,12 +78,16 @@ void driveEnum()
 
     drive += strlen(drive) + 1;
   }
-  printf("%s", finalOutput);
+  return finalOutput;
 }
 
 int main()
 {
-  char* webhookMsg;
-  driveEnum();
+  char* systeminfo = sysInfo();
+  char* drives = driveEnum();
+  printf("%s", drives);
+  printf("%s", systeminfo);
+  free(systeminfo);
+  free(drives);
   return 0;
 }
